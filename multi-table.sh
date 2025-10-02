58248c04aabfb094a12971a1fc9b755f2adce9ba
kdy020819 <kdy020819@gmail.com>
2025-10-02 18:27:29 +0900

    Initial commit: multi_table.sh implement

---
+++ multi-table.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+if [ "$#" -ne 2 ]
+then 
+	echo "Invalid input"
+	exit 1
+fi
+
+row=$1
+col=$2
+
+if [ $row -le 0 ] || [ $col -le 0 ]
+then
+	echo "Input must be greater than 0"
+	exit 1
+fi
+
+i=1
+j=1
+
+until [ $i -gt $row ]
+do
+	until [ $j -gt $col ]
+	do
+		result=`expr $i \* $j`
+		echo -e  "$i*$j=$result \c "
+		j=`expr $j + 1`
+	done
+	
+	echo ""
+	i=`expr $i + 1`
+	j=1
+done
+
+exit 0
